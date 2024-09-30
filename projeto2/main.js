31db730da5523fa8d0a7a9a5eea765d55c4213f1
victorpiressk <victorpiressk@gmail.com>
2024-09-30 20:22:56 -0300

    Atualização projeto 2

---
+++ projeto2/main.js
@@ -0,0 +1,21 @@
+const form = document.getElementById('form-atividade');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+    
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? 'Aprovado' : 'Reprovado'}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+});
