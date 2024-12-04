270097b3efa13a1c4c30b71490984509af6e300e
victorpiressk <victorpiressk@gmail.com>
2024-12-04 16:34:15 -0300

    Finalização projeto loja de carros

--- lojacarros/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#caroussel-imagens').slick({
-        autoplay:true
-    })
-
-    $('.menu-hamburguer').click(function() {
-        $('nav').slideToggle();
-    })
-});
