df00b76d1500e73f49ac9bdccf2b0d925e421d3d
victorpiressk <victorpiressk@gmail.com>
2025-08-05 22:33:29 -0300

    chore: Inclusão do arquivo aula_02_variaveis_e_constantes

---
+++ 003_front_end/005_javascript/aula_02_variaveis_e_constantes.js
@@ -0,0 +1,34 @@
+// Antiga forma de declarar variaveis no JS
+var texte1 = 'ola mundo'
+console.log(texte1)
+
+// let - Os valores das declarações podem ser substituídos
+let texte2 = 'Olá mundo!'
+console.log(texte2)
+
+let fruta
+fruta = 'Banana'
+console.log(fruta)
+
+fruta = 'maçã'
+console.log(fruta)
+
+let falso = fruta == 'Maçã'
+console.log(falso)
+
+let verdade = fruta == 'maçã'
+console.log(verdade)
+
+// const -  Os valores das declarações não podem ser substiuídos e a variável deve ser iniciada com um valor
+const texte3 = 'Olá, mundo!'
+console.log(texte3)
+
+const carro = 'gol'
+console.log(carro)
+
+// carro = 'polo'
+// console.log(carro)
+
+// Utilização do camelCase no nome da variável
+let nomeCompleto = 'Victor José Araújo Pires do Rêgo'
+let calcularDoisNumeros = 2 + 3
