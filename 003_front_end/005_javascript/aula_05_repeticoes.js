6d1de2550dd77a55838c09accb002b5f579ce8c3
victorpiressk <victorpiressk@gmail.com>
2025-08-06 23:11:04 -0300

    chore: Inclusão do arquivo aula_05_repeticoes

---
+++ 003_front_end/005_javascript/aula_05_repeticoes.js
@@ -0,0 +1,13 @@
+let nomes = ['Maria ','José','João']
+
+// Estrutura de repetição for (para)
+for (let i = 0; i < nomes.length; i++) {
+    console.log('O nome é: ' + nomes[i])
+}
+
+// Estrutura de repetição while (enquanto)
+let contador = 0
+while (contador < nomes.length) {
+    console.log('O nome é: ' + nomes[contador])
+    contador++
+}
