6f60211d137dea9af0b3b968c687f29a8d55929d
victorpiressk <victorpiressk@gmail.com>
2025-08-08 06:20:39 -0300

    feat: add lógica da média das notas e finalização do projeto

--- 003_front_end/007_projeto2/main.js
+++ 003_front_end/007_projeto2/main.js
@@ -1,25 +1,62 @@
 const form = document.getElementById('form-atividade')
 const imgAprovado = '<img src="images/aprovado.png" alt="Emoji festejando">'
 const imgReprovado = '<img src="images/reprovado.png" alt="Emoji triste">'
+const atividades = []
+const notas = []
+const spanAprocado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
+
 let linhas = ''
 
 form.addEventListener('submit', function(e) {
     e.preventDefault()
 
+    adicionaLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+})
+
+function adicionaLinha() {
     const inputNomeAtividade = document.getElementById('nome-atividade')
     const inputNotaAtividade = document.getElementById('nota-atividade')
 
-    let linha = '<tr>'
-    linha += `<td>${inputNomeAtividade.value}</td>`
-    linha += `<td>${inputNotaAtividade.value}</td>`
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
-    linha += `</tr>`
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade ${inputNomeAtividade.value} já foi inserida`)
+    } else {
+        atividades.push(inputNomeAtividade.value)
+        notas.push(parseFloat(inputNotaAtividade.value))
 
-    linhas += linha
+        let linha = '<tr>'
+        linha += `<td>${inputNomeAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
+        linha += `</tr>`
+
+        linhas += linha
+    }
     
-    const corpoTabela = document.querySelector('tbody')
-    corpoTabela.innerHTML = linhas
-
     inputNomeAtividade.value = ''
     inputNotaAtividade.value = ''
-})
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal()
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprocado : spanReprovado
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
+}
