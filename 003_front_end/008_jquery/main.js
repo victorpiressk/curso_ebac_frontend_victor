0b07aed48ce0d6792efd7ee1c45aad27321218f0
victorpiressk <victorpiressk@gmail.com>
2025-08-09 00:00:54 -0300

    feat: Inclusão de funcionalidade de adicionar imagens a galeria / Conclusão do projeto

--- 003_front_end/008_jquery/main.js
+++ 003_front_end/008_jquery/main.js
@@ -7,17 +7,30 @@ $(document).ready(function() {
 
     // Forma Jquery de adicionar evento a um elemento
     $('header button').click(function() {
-        $('form').slideDown()
+        $('form').slideToggle()
     })
 
-    $('#btn-cancelar').click(function() {
-        $('form').slideUp()
-    })
+    // $('#btn-cancelar').click(function() {
+    //     $('form').slideUp()
+    // })
 
     // Segunda forma de adicionar evento a um elemento
     // (Retirar comportamento de atualizar a página ao submeter o formulário)
     $('form').on('submit', function(e) {
-        console.log('submit')
         e.preventDefault()
+
+        const enderecoImgNova = $('#endereco-img-nova').val();
+        const novoItem = $('<li style="display: none"></li>')
+        $(`<img src="${enderecoImgNova}" alt="">`).appendTo(novoItem)
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoImgNova}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem)
+        $(novoItem).appendTo('ul')
+        $('#endereco-img-nova').val('')
+        $(novoItem).fadeIn(2000)
     })
 })
