74c5766f4f257f728f990f368e7f42d9c19163db
victorpiressk <victorpiressk@gmail.com>
2025-08-29 20:27:12 -0300

    feat: Configuração de funcionalidade da data do evento / finalização do projeto

--- 003_front_end/021.1_landing_page_evento/src/scripts/main.js
+++ 003_front_end/021.1_landing_page_evento/src/scripts/main.js
@@ -1,1 +1,30 @@
 AOS.init();
+
+const dataDoEvento = new Date("Sep 12, 2025 19:00:00")
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const contaSasHoras = setInterval(function() {
+    const agora = new Date()
+    const timeStampAtual = agora.getTime()
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual
+
+    const diasEmMs = 1000 * 60 * 60 * 24
+    const horasEmMs = 1000 * 60 * 60
+    const minutosEmMs = 1000 * 60
+    const segundosEmMs = 1000
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs)
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs)
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs)
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs)
+
+    console.log(diasAteOEvento, horasAteOEvento, minutosAteOEvento, segundosAteOEvento)
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaSasHoras)
+        document.getElementById('hero-text').innerHTML = 'Evento expirado'
+    }
+}, 1000)
