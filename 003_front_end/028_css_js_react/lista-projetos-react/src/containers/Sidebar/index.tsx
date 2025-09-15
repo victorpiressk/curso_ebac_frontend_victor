07f71fbb005b634c0e8ae6abe730c8e44326659f
victorpiressk <victorpiressk@gmail.com>
2025-09-14 21:32:36 -0300

    chore: Estilizando a sidebar

--- 003_front_end/028_css_js_react/lista-projetos-react/src/containers/Sidebar/index.tsx
+++ 003_front_end/028_css_js_react/lista-projetos-react/src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarConainer } from './styles'
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/victorpiressk.png" alt="" />
-    <Titulo fontSize={20}>Victor Pires</Titulo>
+    <SidebarConainer>
+      <Avatar />
+      <Titulo fontSize={20}>Victor Pires</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        victorpirssk
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarConainer>
   </aside>
 )
 
